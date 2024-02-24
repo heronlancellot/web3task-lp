21b1e47b6afba2cc8318321e133461966ca606ac
heronlancellot <heronlancellot@id.uff.br>
2024-02-24 16:42:06 -0300

    feat: add AccordionUpIcon & AccordionDownIcon

---
+++ src/components/01-atoms/icons/AccordionDownIcon.tsx
@@ -0,0 +1,17 @@
+import React, { SVGProps } from "react";
+
+export const AccordionDownIcon = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		width="19"
+		height="15"
+		viewBox="0 0 19 15"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			id="Vector"
+			d="M9.66117 14.8599L0.82233 2.35986L2.06529 0.602051L9.66117 11.3442L17.257 0.602051L18.5 2.35986L9.66117 14.8599Z"
+			fill="white"
+		/>
+	</svg>
+);
---
+++ src/components/01-atoms/icons/AccordionUpIcon.tsx
@@ -0,0 +1,18 @@
+import React, { SVGProps } from "react";
+
+export const AccordionUpIcon = (props: SVGProps<SVGSVGElement>) => (
+	<svg
+		{...props}
+		width="13"
+		height="8"
+		viewBox="0 0 13 8"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			id="Vector"
+			d="M6.75 0.439941L13 6.68994L12.1211 7.56885L6.75 2.19775L1.37891 7.56885L0.5 6.68994L6.75 0.439941Z"
+			fill="white"
+		/>
+	</svg>
+);
